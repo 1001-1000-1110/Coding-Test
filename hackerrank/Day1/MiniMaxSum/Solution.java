7307a67f7b24b9e980e03559a45a5f02c8c155c7
hoiae <donald819@naver.com>
2023-05-12 21:17:09 +0900

    day1 timeConversion

---
+++ hackerrank/Day1/MiniMaxSum/Solution.java
@@ -0,0 +1,47 @@
+package hackerrank.Day1.MiniMaxSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        Collections.sort(arr);
+        long minSum = 0;
+        for(int i = 0;  i < 4;  i++){
+            minSum  += arr.get(i);
+        }
+        long maxSum = 0;
+        for(int i = 4;  i >= 1;  i--){
+            maxSum += arr.get(i);
+        }
+        System.out.println(minSum+" "+maxSum);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
